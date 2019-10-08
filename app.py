b57645a4409cad822f62060543eef6cede80336a
NAMAN <bansalnaman15@gmail.com>
2019-10-09 01:38:46 +0530

    googlemail.com

--- app.py
+++ app.py
@@ -4,12 +4,11 @@ from flask_mail import Mail, Message
 app = Flask(__name__)
 mail=Mail(app)
 
-app.config['MAIL_SERVER']='smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
+app.config['MAIL_SERVER']='smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
 app.config['MAIL_USERNAME'] = 'nanchalchadwani@gmail.com'
 app.config['MAIL_PASSWORD'] = 'Mayank123'
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
 
 @app.route("/mail")
