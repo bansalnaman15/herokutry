db571b4847ea55877e9930e0cbc1815cfdc53d2e
NAMAN <bansalnaman15@gmail.com>
2019-10-08 23:44:06 +0530

    mail"

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_mail import Mail, Message
 
+app = Flask(__name__)
+mail=Mail(app)
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'nanchalchadwani@gmail.com'
+app.config['MAIL_PASSWORD'] = 'Mayank123'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+
+@app.route("/mail")
+def index():
+   msg = Message('Hello', sender = 'nanchalchadwani@gmail.com', recipients = ['bansalnaman15@gmail.com'])
+   msg.body = "Hello Flask message sent from Flask-Mail Naman"
+   mail.send(msg)
+   return "Sent"
 
 @app.route('/')
 def hello():
     return "Hello World!"
+
+
 
 if __name__ == '__main__':
     app.run()
